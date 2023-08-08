45754b4e30ee560ec4c1f849466ec1b87b746eff
Sineway <sineway@yandex.com>
2023-08-08 11:12:59 +0300

    Добавим FilterView (фильтр точек маршрута)

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import './views/brief-view.js';
+import './views/filter-view.js';
 
 /**
  * @type {import('./views/brief-view').default}
  */
 const briefView = document.querySelector('brief-view');
 
+/**
+ * @type {import('./views/filter-view').default}
+ */
+const filterView = document.querySelector('filter-view');
+
 briefView.render();
+filterView.render();
