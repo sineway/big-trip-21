da05223a5a9e1f54d6d6f6925e8e22215d5beefc
Sineway <sineway@yandex.com>
2023-08-08 11:20:57 +0300

    Добавим AddButtonView (кнопка добавления точки маршрута)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './views/brief-view.js';
 import './views/filter-view.js';
+import './views/add-button-view.js';
 
 /**
  * @type {import('./views/brief-view').default}
@@ -11,5 +12,11 @@ const briefView = document.querySelector('brief-view');
  */
 const filterView = document.querySelector('filter-view');
 
+/**
+ * @type {import('./views/add-button-view').default}
+ */
+const addButtonView = document.querySelector('add-button-view');
+
 briefView.render();
 filterView.render();
+addButtonView.render();
