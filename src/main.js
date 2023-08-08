1201520f12fdbb5cf3abbbb617409ee4de7c5f1a
Sineway <sineway@yandex.com>
2023-08-08 11:26:21 +0300

    Добавим SortView (сортировка точек маршрута)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './views/brief-view.js';
 import './views/filter-view.js';
 import './views/add-button-view.js';
+import './views/sort-view.js';
 
 /**
  * @type {import('./views/brief-view').default}
@@ -17,6 +18,12 @@ const filterView = document.querySelector('filter-view');
  */
 const addButtonView = document.querySelector('add-button-view');
 
+/**
+ * @type {import('./views/sort-view').default}
+ */
+const sortView = document.querySelector('sort-view');
+
 briefView.render();
 filterView.render();
 addButtonView.render();
+sortView.render();
