1f78016832d8eeeada30e7d00dc47e869ce17559
Sineway <sineway@yandex.com>
2023-08-10 19:32:34 +0300

    Опишем типы имеющихся данных

---
+++ src/types.js
@@ -0,0 +1,57 @@
+/**
+ * @typedef {{
+ *  id: string
+ *  base_price: number
+ *  date_from: string
+ *  date_to: string
+ *  destination: string
+ *  is_favorite: boolean
+ *  offers: Array<string>
+ *  type: PointType
+ * }} Point
+ */
+
+/**
+ * @typedef {{
+ *  id: string
+ *  name: string
+ *  description: string
+ *  pictures: Array<Picture>
+ * }} Destination
+ */
+
+/**
+ * @typedef {{
+ *  src: string
+ *  description: string
+ * }} Picture
+ */
+
+/**
+ * @typedef {{
+ *  type: PointType
+ *  offers: Array<Offer>
+ * }} OfferGroup
+ */
+
+/**
+ * @typedef {{
+ *  id: string
+ *  title: string
+ *  price: number
+ * }} Offer
+ */
+
+/**
+ * @typedef {(
+ *   'taxi'
+ * | 'bus'
+ * | 'train'
+ * | 'ship'
+ * | 'drive'
+ * | 'flight'
+ * | 'check-in'
+ * | 'sightseeing'
+ * | 'restaurant'
+ * )} PointType
+ */
