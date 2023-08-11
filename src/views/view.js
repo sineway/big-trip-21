b3753c42182bc49b4f409f325c3b6513ca141c2e
Sineway <sineway@yandex.com>
2023-08-11 08:37:23 +0300

    Добавим базовому View метод setState
    Используя этот метод, Presenter сможет передавать во View 
    данные для отображения (State).
    
    Это позволит ограничить Presenter в знании структуры View (DOM-дерева).
    
    Иначе говоря, можно будет менять разметку View и эти изменения, 
    с большой вероятностью, никак не затронут Presenter.

--- src/views/view.js
+++ src/views/view.js
@@ -1,4 +1,23 @@
+/**
+ * @template State
+ */
 class View extends HTMLElement {
+  constructor() {
+    super();
+
+    /**
+     * @type {State}
+     */
+    this.state = Object.create(null);
+  }
+
+  /**
+   * @param {Partial<State>} state
+   */
+  setState(state) {
+    Object.assign(this.state, state);
+    this.render();
+  }
 
   render() {
     this.innerHTML = this.createHtml();
