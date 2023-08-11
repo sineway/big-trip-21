6bf91df263d839d1f9295097359a8fa82847df94
Sineway <sineway@yandex.com>
2023-08-11 11:39:56 +0300

    Подставим данные в EditorView

--- src/views/editor-view.js
+++ src/views/editor-view.js
@@ -40,11 +40,18 @@ class EditorView extends View {
    * @returns {string}
    */
   createTypeFieldHtml() {
+    const {types} = this.state;
+
     return html`
       <div class="event__type-wrapper">
         <label class="event__type  event__type-btn" for="event-type-toggle-1">
           <span class="visually-hidden">Choose event type</span>
-          <img class="event__type-icon" width="17" height="17" src="img/icons/flight.png" alt="Event type icon">
+          <img
+            class="event__type-icon"
+            width="17"
+            height="17"
+            src="img/icons/${types.find((type) => type.isSelected).value}.png"
+            alt="Event type icon">
         </label>
         <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
 
@@ -52,10 +59,22 @@ class EditorView extends View {
           <fieldset class="event__type-group">
             <legend class="visually-hidden">Event type</legend>
 
-            <div class="event__type-item">
-              <input id="event-type-taxi-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="taxi">
-              <label class="event__type-label  event__type-label--taxi" for="event-type-taxi-1">Taxi</label>
-            </div>
+            ${types.map((type) => html`
+              <div class="event__type-item">
+                <input
+                  id="event-type-${type.value}-1"
+                  class="event__type-input  visually-hidden"
+                  type="radio"
+                  name="event-type"
+                  value="${type.value}"
+                  ${type.isSelected ? 'checked' : ''}>
+                <label
+                  class="event__type-label  event__type-label--${type.value}"
+                  for="event-type-${type.value}-1">
+                  ${type.value}
+                </label>
+              </div>
+            `)}
           </fieldset>
         </div>
       </div>
@@ -66,16 +85,26 @@ class EditorView extends View {
    * @returns {string}
    */
   createDestinationFieldHtml() {
+    const {types, destinations} = this.state;
+
     return html`
       <div class="event__field-group  event__field-group--destination">
         <label class="event__label  event__type-output" for="event-destination-1">
-          Flight
+          ${types.find((type) => type.isSelected).value}
         </label>
-        <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="Geneva" list="destination-list-1">
+
+        <input
+          class="event__input  event__input--destination"
+          id="event-destination-1"
+          type="text"
+          name="event-destination"
+          value="${destinations.find((destination) => destination.isSelected)?.name}"
+          list="destination-list-1">
+
         <datalist id="destination-list-1">
-          <option value="Amsterdam"></option>
-          <option value="Geneva"></option>
-          <option value="Chamonix"></option>
+          ${destinations.map((destination) => html`
+            <option value="${destination.name}"></option>
+          `)}
         </datalist>
       </div>
     `;
@@ -85,13 +114,25 @@ class EditorView extends View {
    * @returns {string}
    */
   createScheduleFieldHtml() {
+    const {dateFrom, dateTo} = this.state;
+
     return html`
       <div class="event__field-group  event__field-group--time">
         <label class="visually-hidden" for="event-start-time-1">From</label>
-        <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="19/03/19 00:00">
+        <input
+          class="event__input  event__input--time"
+          id="event-start-time-1"
+          type="text"
+          name="event-start-time"
+          value="${dateFrom}">
         —
         <label class="visually-hidden" for="event-end-time-1">To</label>
-        <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="19/03/19 00:00">
+        <input
+          class="event__input  event__input--time"
+          id="event-end-time-1"
+          type="text"
+          name="event-end-time"
+          value="${dateTo}">
       </div>
     `;
   }
@@ -100,13 +141,20 @@ class EditorView extends View {
    * @returns {string}
    */
   createPriceFieldHtml() {
+    const {basePrice} = this.state;
+
     return html`
       <div class="event__field-group  event__field-group--price">
         <label class="event__label" for="event-price-1">
           <span class="visually-hidden">Price</span>
           €
         </label>
-        <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="">
+        <input
+          class="event__input  event__input--price"
+          id="event-price-1"
+          type="text"
+          name="event-price"
+          value="${basePrice}">
       </div>
     `;
   }
@@ -144,28 +192,34 @@ class EditorView extends View {
    * @returns {string}
    */
   createOfferListFieldHtml() {
+    const {offers} = this.state;
+
+    if (!offers.length) {
+      return '';
+    }
+
     return html`
       <section class="event__section  event__section--offers">
         <h3 class="event__section-title  event__section-title--offers">Offers</h3>
 
         <div class="event__available-offers">
-          <div class="event__offer-selector">
-            <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" checked="">
-            <label class="event__offer-label" for="event-offer-luggage-1">
-              <span class="event__offer-title">Add luggage</span>
-              +€&nbsp;
-              <span class="event__offer-price">30</span>
-            </label>
-          </div>
+          ${offers.map((offer) => html`
+            <div class="event__offer-selector">
+              <input
+                class="event__offer-checkbox  visually-hidden"
+                id="${offer.id}"
+                type="checkbox"
+                name="event-offer"
+                value="${offer.id}"
+                ${offer.isSelected ? 'checked' : ''}>
 
-          <div class="event__offer-selector">
-            <input class="event__offer-checkbox  visually-hidden" id="event-offer-comfort-1" type="checkbox" name="event-offer-comfort">
-            <label class="event__offer-label" for="event-offer-comfort-1">
-              <span class="event__offer-title">Switch to comfort class</span>
-              +€&nbsp;
-              <span class="event__offer-price">100</span>
-            </label>
-          </div>
+              <label class="event__offer-label" for="${offer.id}">
+                <span class="event__offer-title">${offer.title}</span>
+                +€&nbsp;
+                <span class="event__offer-price">${offer.price}</span>
+              </label>
+            </div>
+          `)}
         </div>
       </section>
     `;
@@ -175,18 +229,23 @@ class EditorView extends View {
    * @returns {string}
    */
   createDestinationHtml() {
+    const {destinations} = this.state;
+    const selectedDestination = destinations.find((destination) => destination.isSelected);
+
+    if (!selectedDestination) {
+      return '';
+    }
+
     return html`
       <section class="event__section  event__section--destination">
         <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-        <p class="event__destination-description">Geneva is a city in Switzerland that lies at the southern tip of expansive Lac Léman (Lake Geneva). Surrounded by the Alps and Jura mountains, the city has views of dramatic Mont Blanc.</p>
+        <p class="event__destination-description">${selectedDestination.description}</p>
 
         <div class="event__photos-container">
           <div class="event__photos-tape">
-            <img class="event__photo" src="img/photos/1.jpg" alt="Event photo">
-            <img class="event__photo" src="img/photos/2.jpg" alt="Event photo">
-            <img class="event__photo" src="img/photos/3.jpg" alt="Event photo">
-            <img class="event__photo" src="img/photos/4.jpg" alt="Event photo">
-            <img class="event__photo" src="img/photos/5.jpg" alt="Event photo">
+            ${selectedDestination.pictures.map((picture) => html`
+              <img class="event__photo" src="${picture.src}" alt="${picture.description}">
+            `)}
           </div>
         </div>
       </section>
