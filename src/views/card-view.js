b2a9ca5ca2a5b199f6350937a1167a8acc804ef6
Sineway <sineway@yandex.com>
2023-08-14 14:46:27 +0300

    Подставим данные в CardView

--- src/views/card-view.js
+++ src/views/card-view.js
@@ -1,5 +1,5 @@
 import View from './view.js';
-import {html} from '../utilities.js';
+import {html, formatDate, formatTime, formatDuration, formatNumber} from '../utilities.js';
 
 /**
  * @typedef {import('./list-view').ItemState} State
@@ -35,8 +35,10 @@ class CardView extends View {
    * @returns {string}
    */
   createStartDateHtml() {
+    const {dateFrom} = this.state;
+
     return html`
-      <time class="event__date" datetime="2019-03-18">MAR 18</time>
+      <time class="event__date" datetime="${dateFrom}">${formatDate(dateFrom)}</time>
     `;
   }
 
@@ -44,9 +46,16 @@ class CardView extends View {
    * @returns {string}
    */
   createTypeIconHtml() {
+    const {types} = this.state;
+
     return html`
       <div class="event__type">
-        <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+        <img
+          class="event__type-icon"
+          width="42"
+          height="42"
+          src="img/icons/${types.find((type) => type.isSelected).value}.png"
+          alt="Event type icon">
       </div>
     `;
   }
@@ -55,8 +64,12 @@ class CardView extends View {
    * @returns {string}
    */
   createDestinationHtml() {
+    const {types, destinations} = this.state;
+    const selectedType = types.find((type) => type.isSelected);
+    const selectedDestination = destinations.find((destination) => destination.isSelected);
+
     return html`
-      <h3 class="event__title">Taxi Amsterdam</h3>
+      <h3 class="event__title">${selectedType.value} ${selectedDestination.name}</h3>
     `;
   }
 
@@ -64,14 +77,16 @@ class CardView extends View {
    * @returns {string}
    */
   createScheduleHtml() {
+    const {dateFrom, dateTo} = this.state;
+
     return html`
       <div class="event__schedule">
         <p class="event__time">
-          <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+          <time class="event__start-time" datetime="${dateFrom}">${formatTime(dateFrom)}</time>
           —
-          <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+          <time class="event__end-time" datetime="${dateTo}">${formatTime(dateTo)}</time>
         </p>
-        <p class="event__duration">30M</p>
+        <p class="event__duration">${formatDuration(dateFrom, dateTo)}</p>
       </div>
     `;
   }
@@ -80,9 +95,11 @@ class CardView extends View {
    * @returns {string}
    */
   createPriceHtml() {
+    const {basePrice} = this.state;
+
     return html`
       <p class="event__price">
-        €&nbsp;<span class="event__price-value">20</span>
+        €&nbsp;<span class="event__price-value">${formatNumber(basePrice)}</span>
       </p>
     `;
   }
@@ -91,14 +108,23 @@ class CardView extends View {
    * @returns {string}
    */
   createOfferListHtml() {
+    const {offers} = this.state;
+    const selectedOffers = offers.filter((offer) => offer.isSelected);
+
+    if (!selectedOffers.length) {
+      return '';
+    }
+
     return html`
       <h4 class="visually-hidden">Offers:</h4>
       <ul class="event__selected-offers">
-        <li class="event__offer">
-          <span class="event__offer-title">Order Uber</span>
-          +€&nbsp;
-          <span class="event__offer-price">20</span>
-        </li>
+        ${selectedOffers.map((offer) => html`
+          <li class="event__offer">
+            <span class="event__offer-title">${offer.title}</span>
+            +€&nbsp;
+            <span class="event__offer-price">${offer.price}</span>
+          </li>
+        `)}
       </ul>
     `;
   }
@@ -107,8 +133,10 @@ class CardView extends View {
    * @returns {string}
    */
   createFavoriteButtonHtml() {
+    const {isFavorite} = this.state;
+
     return html`
-      <button class="event__favorite-btn event__favorite-btn--active" type="button">
+      <button class="event__favorite-btn ${isFavorite ? 'event__favorite-btn--active' : ''}" type="button">
         <span class="visually-hidden">Add to favorite</span>
         <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
           <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"></path>
