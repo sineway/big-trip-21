934d7bb932794db6aacf752063bbf7c8b9f8d6dc
Sineway <sineway@yandex.com>
2023-08-18 09:52:42 +0300

    Подставим данные в FilterView

--- src/views/filter-view.js
+++ src/views/filter-view.js
@@ -26,25 +26,23 @@ class FilterView extends View {
    */
   createHtml() {
     return html`
-      <div class="trip-filters__filter">
-        <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked="">
-        <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-      </div>
-
-      <div class="trip-filters__filter">
-        <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
-        <label class="trip-filters__filter-label" for="filter-future">Future</label>
-      </div>
-
-      <div class="trip-filters__filter">
-        <input id="filter-present" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="present">
-        <label class="trip-filters__filter-label" for="filter-present">Present</label>
-      </div>
-
-      <div class="trip-filters__filter">
-        <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
-        <label class="trip-filters__filter-label" for="filter-past">Past</label>
-      </div>
+      ${this.state.items.map((item) => html`
+        <div class="trip-filters__filter">
+          <input
+            id="filter-${item.value}"
+            class="trip-filters__filter-input  visually-hidden"
+            type="radio"
+            name="trip-filter"
+            value="${item.value}"
+            ${item.isSelected ? 'checked' : ''}
+            ${item.isDisabled ? 'disabled' : ''}>
+          <label
+            class="trip-filters__filter-label"
+            for="filter-${item.value}">
+            ${item.value}
+          </label>
+        </div>
+      `)}
     `;
   }
 }
