7f32ec5fedc33c5c993b1f78dcdf442ea33f4e4c
Sineway <sineway@yandex.com>
2023-08-18 10:04:23 +0300

    Подставим данные в SortView

--- src/views/sort-view.js
+++ src/views/sort-view.js
@@ -26,30 +26,23 @@ class SortView extends View {
    */
   createHtml() {
     return html`
-      <div class="trip-sort__item  trip-sort__item--day">
-        <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" checked="">
-        <label class="trip-sort__btn" for="sort-day">Day</label>
-      </div>
-
-      <div class="trip-sort__item  trip-sort__item--event">
-        <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" disabled="">
-        <label class="trip-sort__btn" for="sort-event">Event</label>
-      </div>
-
-      <div class="trip-sort__item  trip-sort__item--time">
-        <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
-        <label class="trip-sort__btn" for="sort-time">Time</label>
-      </div>
-
-      <div class="trip-sort__item  trip-sort__item--price">
-        <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
-        <label class="trip-sort__btn" for="sort-price">Price</label>
-      </div>
-
-      <div class="trip-sort__item  trip-sort__item--offer">
-        <input id="sort-offer" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-offer" disabled="">
-        <label class="trip-sort__btn" for="sort-offer">Offers</label>
-      </div>
+      ${this.state.items.map((item) => html`
+        <div class="trip-sort__item  trip-sort__item--${item.value}">
+          <input
+            id="sort-${item.value}"
+            class="trip-sort__input  visually-hidden"
+            type="radio"
+            name="trip-sort"
+            value="${item.value}"
+            ${item.isSelected ? 'checked' : ''}
+            ${item.isDisabled ? 'disabled' : ''}>
+          <label
+            class="trip-sort__btn"
+            for="sort-${item.value}">
+            ${item.value}
+          </label>
+        </div>
+      `)}
     `;
   }
 }
