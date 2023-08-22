2c668c0f1668b8cb6e7a0825b8f9632dd4bbec4d
Sineway <sineway@yandex.com>
2023-08-22 12:20:33 +0300

    Добавим в ListPresenter обработчик события favorite (WIP)

--- src/presenters/list-presenter.js
+++ src/presenters/list-presenter.js
@@ -15,6 +15,7 @@ class ListPresenter extends Presenter {
 
     this.view.addEventListener('open', this.onViewOpen.bind(this));
     this.view.addEventListener('close', this.onViewClose.bind(this));
+    this.view.addEventListener('favorite', this.onViewFavorite.bind(this));
   }
 
   /**
@@ -78,6 +79,19 @@ class ListPresenter extends Presenter {
     delete params.edit;
 
     this.navigation.setParams(params);
+  }
+
+  /**
+   * @param {CustomEvent & {
+   *  target: import('../views/card-view').default
+   * }} event
+   */
+  onViewFavorite(event) {
+    const card = event.target;
+
+    card.state.isFavorite = !card.state.isFavorite;
+    // TODO: Обновить модель
+    card.render();
   }
 }
 
