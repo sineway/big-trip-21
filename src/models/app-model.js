0d03579daf8fb70b4bf883752c57b10e36cdb02a
Sineway <sineway@yandex.com>
2023-08-27 20:29:13 +0300

    Реализуем сортировку по цене

--- src/models/app-model.js
+++ src/models/app-model.js
@@ -30,7 +30,7 @@ class AppModel extends Model {
       day: () => 0,
       event: () => 0,
       time: () => 0,
-      price: () => 0,
+      price: (pointA, pointB) => pointB.basePrice - pointA.basePrice,
       offers: () => 0
     };
   }
@@ -45,6 +45,9 @@ class AppModel extends Model {
     this.destinations = destinations;
     // @ts-ignore
     this.offerGroups = offerGroups;
+    console.table(
+      this.getPoints({sort: 'price'})
+    );
   }
 
   /**
