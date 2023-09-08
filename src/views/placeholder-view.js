eb683ffa07352d6aac805f26193aac40005027aa
Sineway <sineway@yandex.com>
2023-09-08 09:17:49 +0300

    Научим PlaceholderView принимать данные

--- src/views/placeholder-view.js
+++ src/views/placeholder-view.js
@@ -1,13 +1,26 @@
 import View from './view.js';
 import {html} from '../utilities.js';
 
+/**
+ * @typedef {{
+ *  message: string
+ * }} State
+ *
+ * @extends {View<State>}
+ */
 class PlaceholderView extends View {
   /**
    * @override
    */
   createHtml() {
+    const {message} = this.state;
+
+    if (!message) {
+      return '';
+    }
+
     return html`
-      <p class="trip-events__msg">Click New Event to create your first point</p>
+      <p class="trip-events__msg">${message}</p>
     `;
   }
 }
