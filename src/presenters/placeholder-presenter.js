d2b4b406448467f29b0521b06f260adc527b091d
Sineway <sineway@yandex.com>
2023-09-08 09:34:23 +0300

    Сформируем данные для PlaceholderView

--- src/presenters/placeholder-presenter.js
+++ src/presenters/placeholder-presenter.js
@@ -13,14 +13,38 @@ class PlaceholderPresenter extends Presenter {
   constructor(...rest) {
     super(...rest);
 
-    // this.view.addEventListener('change', this.onViewChange.bind(this));
+    /**
+     * @type {Record<FilterType, string>}
+     */
+    this.messages = {
+      everything: 'Click New Event to create your first point',
+      future: 'There are no future events now',
+      present: 'There are no present events now',
+      past: 'There are no past events now'
+    };
   }
 
   /**
    * @override
    */
   updateView() {
-    this.view.render();
+    this.view.setState({
+      message: this.getMessage()
+    });
+  }
+
+  /**
+   * @returns {string}
+   */
+  getMessage() {
+    const params = this.navigation.getParams();
+    const points = this.model.getPoints(params);
+
+    if (!points.length) {
+      return this.messages[params.filter] ?? this.messages.everything;
+    }
+
+    return '';
   }
 }
 
