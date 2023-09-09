f29c303a3a765c199c311092222829d532c21709
Sineway <sineway@yandex.com>
2023-09-09 19:38:20 +0300

    Реализуем отрисовку редактора новой точки маршрута (WIP)

--- src/presenters/list-presenter.js
+++ src/presenters/list-presenter.js
@@ -29,6 +29,10 @@ class ListPresenter extends Presenter {
     const destinations = this.model.getDestinations();
     const offerGroups = this.model.getOfferGroups();
 
+    if (params.edit === 'draft') {
+      points.unshift(this.createDraftPoint());
+    }
+
     const items = points.map((point) => {
       const {offers} = offerGroups.find((group) => group.type === point.type);
 
@@ -60,6 +64,22 @@ class ListPresenter extends Presenter {
     });
 
     this.view.setState({items});
+  }
+
+  /**
+   * @returns {import('../models/point-model').default}
+   */
+  createDraftPoint() {
+    const point = this.model.createPoint();
+
+    Object.assign(point, {
+      id: 'draft',
+      type: 'flight',
+      basePrice: 0,
+      isFavorite: false
+    });
+
+    return point;
   }
 
   /**
