08f7181cccb952d3d0e47b10685557621dce856c
Sineway <sineway@yandex.com>
2023-09-09 19:12:32 +0300

    Научим AddButtonView принимать данные

--- src/views/add-button-view.js
+++ src/views/add-button-view.js
@@ -1,13 +1,27 @@
 import View from './view.js';
 import {html} from '../utilities.js';
 
+/**
+ * @typedef {{
+ *  isDisabled: boolean
+ * }} State
+ *
+ * @extends {View<State>}
+ */
 class AddButtonView extends View {
   /**
    * @override
    */
   createHtml() {
+    const {isDisabled} = this.state;
+
     return html`
-      <button class="trip-main__event-add-btn  btn  btn--big  btn--yellow" type="button" disabled="">New event</button>
+      <button
+        class="trip-main__event-add-btn  btn  btn--big  btn--yellow"
+        type="button"
+        ${isDisabled ? 'disabled' : ''}>
+        New event
+      </button>
     `;
   }
 }
