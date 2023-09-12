4e00ab87f0f20dcb3f31115836041a35c60fb978
Sineway <sineway@yandex.com>
2023-09-12 22:39:28 +0300

    Добавим функцию для экранирования данных

--- src/utilities.js
+++ src/utilities.js
@@ -2,6 +2,7 @@ import 'flatpickr/dist/flatpickr.css';
 import flatpickr from 'flatpickr';
 import dayjs from 'dayjs';
 import durationPlugin from 'dayjs/plugin/duration.js';
+import {escape} from 'he';
 
 dayjs.extend(durationPlugin);
 
@@ -100,11 +101,36 @@ function html(strings, ...values) {
   });
 }
 
+/**
+ * @param {any} data
+ * @returns {any}
+ */
+function sanitize(data) {
+  switch (data?.constructor) {
+    case String:
+      return escape(data);
+
+    case Array:
+      return data.map(sanitize);
+
+    case Object:
+      return Object.keys(data).reduce((copy, key) => {
+        copy[key] = sanitize(data[key]);
+
+        return copy;
+      }, {});
+
+    default:
+      return data;
+  }
+}
+
 export {
   createCalendars,
   formatDate,
   formatTime,
   formatDuration,
   formatNumber,
-  html
+  html,
+  sanitize
 };
