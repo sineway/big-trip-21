223eb8dfdde282946b3f27d25828460db4bd21db
Sineway <sineway@yandex.com>
2023-09-14 12:37:42 +0300

    Добавим в базовую Model методы для работы с событиями

--- src/models/model.js
+++ src/models/model.js
@@ -1,3 +1,17 @@
-class Model {}
+class Model extends EventTarget {
+  /**
+   * @param {string} type
+   * @param {any} detail
+   * @returns {boolean}
+   */
+  dispatch(type, detail = null) {
+    const event = new CustomEvent(type, {
+      detail,
+      cancelable: true
+    });
+
+    return this.dispatchEvent(event);
+  }
+}
 
 export default Model;
