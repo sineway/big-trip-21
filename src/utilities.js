2e9c432fead5fde4b183009ea05c91d528644913
Sineway <sineway@yandex.com>
2023-09-20 18:27:22 +0300

    Добавим функцию formatList

--- src/utilities.js
+++ src/utilities.js
@@ -81,6 +81,20 @@ function formatNumber(value) {
 }
 
 /**
+ * @param {Array<string>} items
+ * @returns {string}
+ */
+function formatList(items) {
+  items = structuredClone(items);
+
+  if (items.length > 3) {
+    items.splice(1, items.length - 2, '...');
+  }
+
+  return items.join(' — ');
+}
+
+/**
  * @param {TemplateStringsArray} strings
  * @param {...any} values
  * @returns {string}
@@ -131,6 +145,7 @@ export {
   formatTime,
   formatDuration,
   formatNumber,
+  formatList,
   html,
   sanitize
 };
